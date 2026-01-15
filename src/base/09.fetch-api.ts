47967af6b037a111ebe4f4dffad47a1caaaa82d0
DaniHDZM <sayu142184@gmail.com>
2026-01-15 12:48:22 -0600

    Interface fetch

--- src/base/09.fetch-api.ts
+++ src/base/09.fetch-api.ts
@@ -1,6 +1,8 @@
+import type { GIFResponse } from "../interfaces/git-response"
+
 const apiKey = 'G02R1WyM0omqWazMNnJ4HOl9O6LplnKq'
 
 fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
-    .then( response => response.json())
-    .then(body => console.log({ body })) // los { body } sirve para desestructurar 
-    .catch( err => console.log(err))
+    .then( (response) => response.json())
+    .then((body: GIFResponse) => console.log( body.data.images.downsized_still.url )) // los { body } sirve para desestructurar 
+    .catch( (err) => console.log(err))
